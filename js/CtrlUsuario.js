a527c9f8fdd18b9e3cce9371bfdbe5e31b196c00
InkryCompany <86129178+InkryCompany@users.noreply.github.com>
2021-06-20 00:57:46 -0500

    Add files via upload

---
+++ js/CtrlUsuario.js
@@ -0,0 +1,68 @@
+import {
+  getAuth, getFirestore
+} from "../lib/fabrica.js";
+import {
+  eliminaStorage, urlStorage
+} from "../lib/storage.js";
+import {
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraUsuarios
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+import {
+  checksRoles, guardaUsuario, selectPasatiempos
+} from "./usuarios.js";
+
+const params = new URL(location.href).searchParams;
+const id = params.get("id");
+const daoUsuario = getFirestore().collection("Usuario");
+const forma = document["forma"];
+const img = document.querySelector("img");
+const listaRoles = document.querySelector("#listaRoles");
+getAuth().onAuthStateChanged(protege, muestraError);
+
+async function protege(usuario) {
+  if (tieneRol(usuario, ["Administrador"])) {
+    busca();
+  }
+}
+
+async function busca() {
+  try {
+    const doc = await daoUsuario.doc(id).get();
+    if (doc.exists) {
+      const data = doc.data();
+      forma.cue.value = id || "";
+      img.src = await urlStorage(id);
+      selectPasatiempos(
+        forma.pasatiempoId,
+        data.pasatiempoId)
+      checksRoles(listaRoles, data.rolIds);
+      forma.addEventListener("submit", guarda);
+      forma.eliminar.addEventListener("click", elimina);
+    }
+  } catch (e) {
+    muestraError(e);
+    muestraUsuarios();
+  }
+}
+
+async function guarda(evt) {
+  await guardaUsuario(evt, new FormData(forma), id);
+}
+
+async function elimina() {
+  try {
+    if (confirm("Confirmar la " + "eliminación")) {
+      await daoUsuario.doc(id).delete();
+      await eliminaStorage(id);
+      muestraUsuarios();
+    }
+  } catch (e) {
+    muestraError(e);
+  }
+}
---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDMx0bZt_aGnQGxiJPiYmCFsGPGRK1ulgM",
+  authDomain: "fbinkrycompanyt.firebaseapp.com",
+  projectId: "fbinkrycompanyt",
+  storageBucket: "fbinkrycompanyt.appspot.com",
+  messagingSenderId: "681438004764",
+  appId: "1:681438004764:web:0acdc2fb31803122ee81d9",
+  measurementId: "G-LRLGG38TFW"
+});
